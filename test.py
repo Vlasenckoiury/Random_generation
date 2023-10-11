89af1a2b540e2333bc9a9935f1bb2ef62bc40210
vlase <vlasencko_iury1999@yandex.by>
2023-10-11 10:54:46 +0300

    заполнение авто формы

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+
+GoogleURL = 'http://over.org.tilda.ws/testuser'
+
+urlResponse = GoogleURL + '/formResponse'
+urlReferer = GoogleURL + 'viewform?edit_requested=true'
+
+name = 'Iury'
+email = 'Iury458748@yandex.by'
+number = '+375444784523'
+
+form_data = {'entry.2005620554': name,
+             'entry.1045781291': email,
+             'entry.1166974658': number,
+             }
+user_agent = {'Referer': urlReferer,
+              'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
+              # 'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
+r = requests.post(urlResponse, data=form_data, headers=user_agent)
